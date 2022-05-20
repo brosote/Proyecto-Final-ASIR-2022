83fbe4d7c623867dcddfd0521cca7610e303aedc
brosote <92148822+brosote@users.noreply.github.com>
2022-05-20 19:39:34 +0200

    Update aprobar.php

--- aprobar.php
+++ aprobar.php
@@ -3,7 +3,7 @@
 	include "db_conn.php";
 	$idAlumno = $_GET['idAlumno'];
 	
-	$sql = "UPDATE matricula SET estado = 'Aceptada' WHERE idAlumno = '$idAlumno'";
+	$sql = "UPDATE Matricula SET estado = 'Aceptada' WHERE idAlumno = '$idAlumno'";
 	$aprobar = mysqli_query($connection,$sql);
 	
 	header("location:gestionmatriculas.php");
