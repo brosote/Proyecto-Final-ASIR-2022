d21acc795fce99bd189ce71bea0d546fcbc8fdc8
brosote <92148822+brosote@users.noreply.github.com>
2022-05-20 19:07:47 +0200

    Update datosVelez.sql

--- datosVelez.sql
+++ datosVelez.sql
@@ -1,21 +1,21 @@
 -- Datos catálogo localidad:
-INSERT INTO `velez`.`localidad` (`cp`, `Localidad`, `Provincia`) VALUES ('14140', 'La Victoria', 'Córdoba');
-INSERT INTO `velez`.`localidad` (`cp`, `Localidad`, `Provincia`) VALUES ('41400', 'Ecija', 'Sevilla');
-INSERT INTO `velez`.`localidad` (`cp`, `Localidad`, `Provincia`) VALUES ('41560', 'Estepa', 'Sevilla');
-INSERT INTO `velez`.`localidad` (`cp`, `Localidad`, `Provincia`) VALUES ('41640', 'Osuna', 'Sevilla');
-INSERT INTO `velez`.`localidad` (`cp`, `Localidad`, `Provincia`) VALUES ('41630', 'Lantejuela', 'Sevilla');
+INSERT INTO `localidad` (`cp`, `Localidad`, `Provincia`) VALUES ('14140', 'La Victoria', 'Córdoba');
+INSERT INTO `localidad` (`cp`, `Localidad`, `Provincia`) VALUES ('41400', 'Ecija', 'Sevilla');
+INSERT INTO `localidad` (`cp`, `Localidad`, `Provincia`) VALUES ('41560', 'Estepa', 'Sevilla');
+INSERT INTO `localidad` (`cp`, `Localidad`, `Provincia`) VALUES ('41640', 'Osuna', 'Sevilla');
+INSERT INTO `localidad` (`cp`, `Localidad`, `Provincia`) VALUES ('41630', 'Lantejuela', 'Sevilla');
 
 -- Datos catálogo ciclos:
-INSERT INTO `velez`.`ciclos` (`idCiclo`, `nombre`) VALUES ('DAW', 'Desarrollo de Aplicaciones Web');
-INSERT INTO `velez`.`ciclos` (`idCiclo`, `nombre`) VALUES ('DAM', 'Desarrollo de Aplicaciones Multiplataforma');
-INSERT INTO `velez`.`ciclos` (`idCiclo`, `nombre`) VALUES ('ASIR', 'Administración Sistemas Informáticos en Red');
-INSERT INTO `velez`.`ciclos` (`idCiclo`, `nombre`) VALUES ('SMR', 'Sistemas Microinformáticos en Red');
+INSERT INTO `ciclos` (`idCiclo`, `nombre`) VALUES ('DAW', 'Desarrollo de Aplicaciones Web');
+INSERT INTO `ciclos` (`idCiclo`, `nombre`) VALUES ('DAM', 'Desarrollo de Aplicaciones Multiplataforma');
+INSERT INTO `ciclos` (`idCiclo`, `nombre`) VALUES ('ASIR', 'Administración Sistemas Informáticos en Red');
+INSERT INTO `ciclos` (`idCiclo`, `nombre`) VALUES ('SMR', 'Sistemas Microinformáticos en Red');
 
 -- Datos de asignaturas disponibles para la matrícula.
-INSERT INTO `velez`.`asignatura` (`idAsignatura`, `nombreAsignatura`, `curso`, `idCiclo`) VALUES ('1', 'Lenguajes de Marcas', 'primero', 'DAW');
-INSERT INTO `velez`.`asignatura` (`idAsignatura`, `nombreAsignatura`, `curso`, `idCiclo`) VALUES ('2', 'Redes', 'primero', 'ASIR');
-INSERT INTO `velez`.`asignatura` (`idAsignatura`, `nombreAsignatura`, `curso`, `idCiclo`) VALUES ('3', 'IMWEB', 'segundo', 'ASIR');
-INSERT INTO `velez`.`asignatura` (`idAsignatura`, `nombreAsignatura`, `curso`, `idCiclo`) VALUES ('4', 'ADSGB', 'segundo', 'ASIR');
+INSERT INTO `asignatura` (`idAsignatura`, `nombreAsignatura`, `curso`, `idCiclo`) VALUES ('1', 'Lenguajes de Marcas', 'primero', 'DAW');
+INSERT INTO `asignatura` (`idAsignatura`, `nombreAsignatura`, `curso`, `idCiclo`) VALUES ('2', 'Redes', 'primero', 'ASIR');
+INSERT INTO `asignatura` (`idAsignatura`, `nombreAsignatura`, `curso`, `idCiclo`) VALUES ('3', 'IMWEB', 'segundo', 'ASIR');
+INSERT INTO `asignatura` (`idAsignatura`, `nombreAsignatura`, `curso`, `idCiclo`) VALUES ('4', 'ADSGB', 'segundo', 'ASIR');
 
 -- Datos de alumnos
 INSERT INTO `alumno` (`idAlumno`, `nombre`, `apellido1`, `apellido2`, `fecNacimiento`, `sexo`, `correo`, `nacionalidad`, `cp`, `direccion`) VALUES ('11111111A', 'Jose', 'Suarez', 'Ruiz', '1990/05/12', 'm', 'jsuru@g.es', 'española', '14140', 'avd la paz 2');
